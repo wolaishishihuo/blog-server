4ee887f7f48ffcc38002a4f488e9bc1d50f93dbf
jiazhigang <jiazhigang_888@163.com>
2025-01-10 17:28:53 +0800

    feat: 完成登录功能

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterAuthDto } from './dto/register-auth.dto';
+import { LoginAuthDto } from './dto/login-auth.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
     @Post('register')
     register(@Body() registerAuthDto: RegisterAuthDto) {
         return this.authService.register(registerAuthDto);
+    }
+
+    @Post('login')
+    login(@Body() loginAuthDto: LoginAuthDto) {
+        return this.authService.login(loginAuthDto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,15 @@ import { RegisterAuthDto } from './dto/register-auth.dto';
 import { PrismaService } from '@/prisma/prisma.service';
 import * as argon2 from 'argon2';
 import { UserRole } from '@/enum/user';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { JwtService } from '@nestjs/jwt';
+
 @Injectable()
 export class AuthService {
-    constructor(private readonly prisma: PrismaService) {}
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly jwtService: JwtService
+    ) {}
 
     async register(registerAuthDto: RegisterAuthDto) {
         const { username, password, email, nickname, confirmPassword } = registerAuthDto;
@@ -46,5 +52,39 @@ export class AuthService {
             message: '注册成功!',
             code: HttpStatus.OK
         };
+    }
+
+    async login(loginAuthDto: LoginAuthDto) {
+        const { username, password } = loginAuthDto;
+        const foundUser = await this.prisma.user.findUnique({
+            where: {
+                username
+            }
+        });
+        if (!foundUser) {
+            throw new HttpException('用户名或密码错误!', HttpStatus.OK);
+        }
+
+        const isPasswordValid = await argon2.verify(foundUser.password, password);
+        if (!isPasswordValid) {
+            throw new HttpException('用户名或密码错误!', HttpStatus.OK);
+        }
+        const payload = {
+            username: foundUser.username,
+            email: foundUser.email,
+            id: foundUser.id
+        };
+        const token = await this.getToken(payload);
+        return {
+            message: '登录成功!',
+            code: HttpStatus.OK,
+            data: {
+                accessToken: token
+            }
+        };
+    }
+
+    async getToken(payload: { username: string; email: string; id: number }) {
+        return await this.jwtService.signAsync(payload);
     }
 }
