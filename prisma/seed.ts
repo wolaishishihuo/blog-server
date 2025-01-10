141698705d7945ad300af24b21d352927a011b35
jiazhigang <jiazhigang_888@163.com>
2025-01-10 12:47:21 +0800

    feat: Init

---
+++ prisma/seed.ts
@@ -0,0 +1,14 @@
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// async function main() {
+//     const user = await prisma.user.create({
+//         data: {
+//             username: 'admin',
+//             password: 'admin'
+//         }
+//     });
+// }
+
+// main();
